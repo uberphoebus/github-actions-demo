3321692309261c033e514631b043980c7be4f19f
jaehoon.park-pf <jhpark@vueron.com>
2024-12-31 15:29:17 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import pandas as pd
+
+
+def main():
+    print("안녕하세요, GitHub Actions!")
+    result = add_numbers(5, 7)
+    print(f"5와 7을 더한 결과는: {result}")
+    df = pd.DataFrame({"a": [1, 2, 3], "b": [5, 9, 8]})
+    print(df)
+    print(Path().cwd())
+
+
+def add_numbers(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    main()
