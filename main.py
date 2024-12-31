d29b84b5de7c3368fc4ed2e625a3c9804634981d
jaehoon.park-pf <jhpark@vueron.com>
2024-12-31 16:12:26 +0900

    fail

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     print("안녕하세요, GitHub Actions!")
     result = add_numbers(5, 7)
     print(f"5와 7을 더한 결과는: {result}")
+    raise Exception()
     df = pd.DataFrame({"a": [1, 2, 3], "b": [5, 9, 8]})
     print(df)
     print(Path().cwd())
